644bf27f02c645625353fe166955ba33a0e5c867
Siripiri-hue <daemonprocellae@live.it>
2022-05-11 16:03:15 +0200

    Relazione tra i model

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    // $fillable = ['name', 'slug'];
+    protected $fillable = ['name', 'slug'];
+    
+    public function posts () {
+        return $this->hasMany('App\Post');
+    }
 }
