6873086f940a07b45e938ac0b506b0947e6f07fc
Siripiri-hue <daemonprocellae@live.it>
2022-05-11 19:27:55 +0200

    Merge branch 'laravel-one-to-many' of https://github.com/Siripiri-hue/laravel-one-to-many into laravel-one-to-many

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -28,10 +28,6 @@
             {{-- <td>{{ $post->content }}</td>  --}}
             <td>{{ $post->published_at }}</td> 
             <td>{{ $post->category ? $post->category->name : '' }}</td>
-<<<<<<< HEAD
-=======
-            {{-- <td>{{ $post->category->name }}</td> --}}
->>>>>>> 086b6134aaf44a39f1b8f51b33e81d539a05028d
             <td><a href="{{ route('admin.posts.edit', $post) }}">Modifica post</a></td> 
             <td>
                 <form class="form-group" action="{{ route('admin.posts.destroy', $post) }}" method="POST">
